60dfe5ad0919a06a9df1c00d177c953b04dbae04
Shubhankar_Ranjan <shubhankar515@gmail.com>
2023-03-25 17:58:22 +0530

    Added team members settings at admin panel

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -29,6 +29,13 @@
         return $data;
     }
 
+    function selectAll($table)
+    {
+        $con = $GLOBALS['con'];
+        $res = mysqli_query($con,"SELECT * FROM $table");
+        return $res;
+    }
+
     function select($sql,$values,$datatype)
     {
         $con = $GLOBALS['con'];  // $con has been already declared outside of this function so to call this again we have to use $GLOBALS
@@ -68,6 +75,48 @@
         }
         else{
             die("Query cannot be prepared - Update");
+        }
+    }
+
+    function insert($sql,$values,$datatype)
+    {
+        $con = $GLOBALS['con'];  // $con has been already declared outside of this function so to call this again we have to use $GLOBALS
+        if($stmt = mysqli_prepare($con,$sql))
+        {
+            mysqli_stmt_bind_param($stmt,$datatype,...$values);
+            if(mysqli_stmt_execute($stmt)){
+               $res = mysqli_stmt_affected_rows($stmt);
+               mysqli_stmt_close($stmt);  // it is necessary to close the prepared statements
+               return $res;
+            }
+            else{
+                mysqli_stmt_close($stmt);
+                die("Query cannot be executed - Insert");
+            }
+        }
+        else{
+            die("Query cannot be prepared - Insert");
+        }
+    }
+
+    function delete($sql,$values,$datatype)
+    {
+        $con = $GLOBALS['con'];  // $con has been already declared outside of this function so to call this again we have to use $GLOBALS
+        if($stmt = mysqli_prepare($con,$sql))
+        {
+            mysqli_stmt_bind_param($stmt,$datatype,...$values);
+            if(mysqli_stmt_execute($stmt)){
+               $res = mysqli_stmt_affected_rows($stmt);
+               mysqli_stmt_close($stmt);  // it is necessary to close the prepared statements
+               return $res;
+            }
+            else{
+                mysqli_stmt_close($stmt);
+                die("Query cannot be executed - Delete");
+            }
+        }
+        else{
+            die("Query cannot be prepared - Delete");
         }
     }
 
